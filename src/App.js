86b820dfad86c398f90ab6b1a6398d31aa32ebc4
Chris Camp <cpcamp2@gmail.com>
2018-08-15 21:03:27 -0400

    add basic structure of app jsx

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='app'>
+        <header>
+          <div className='wrapper'>
+            <h1>Potluck</h1>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='container'>
+          <section className='add-item'>
+
+          </section>
+        </div>
+        <section className='display-item'>
+          <div className='wrapper'>
+            <ul>
+            </ul>
+          </div>
+        </section>
       </div>
     );
   }
